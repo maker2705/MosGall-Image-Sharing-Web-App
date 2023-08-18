cd780c5924cd8d236485d3550e457af66822cf94
maker2705 <imankit2705@gmail.com>
2023-08-18 16:47:21 +0530

    #2_added assets

--- frontend/mos_gall/src/App.js
+++ frontend/mos_gall/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Routes, Route, useNavigate } from 'react-router-dom'
+import Home from "./container/Home";
+import Login from "./components/Login";
 function App() {
   return (
-    <h1 className="text-3xl text-amber-500 font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+        <Route path="login" element = {<Login/>} />
+        <Route path="/*" element = {<Home/>} />
+    </Routes>
   );
 }
 
---
+++ frontend/mos_gall/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { GoogleLogin, useGoogleOneTapLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { fcG }
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
